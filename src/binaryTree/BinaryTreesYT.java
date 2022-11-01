335227110ce01aea7fc45b0bfc3ed684f8050db7
saiful <saifulremon>
2022-11-01 23:40:08 +0600

    lever order of a tree done

--- src/binaryTree/BinaryTreesYT.java
+++ src/binaryTree/BinaryTreesYT.java
@@ -1,6 +1,9 @@
 package binaryTree;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreesYT {
     static class Node {
         Node left;
@@ -41,12 +44,41 @@ public class BinaryTreesYT {
             preorder(root.right);
         }
 
+        public static void levelOrder(Node root) {
+            if(root == null) {
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()) {
+                Node curr = q.remove();
+                if(curr == null) {
+                    System.out.println();
+                    //queue empty
+                    if(q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(curr.data+" ");
+                    if(curr.left != null) {
+                        q.add(curr.left);
+                    }
+                    if(curr.right != null) {
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
+
         public static void main(String[] args) {
             int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
             BinaryTree tree = new BinaryTree();
             Node root = tree.buildTree(nodes);
-            preorder(root);
+           levelOrder(root);
         }
     }
 
